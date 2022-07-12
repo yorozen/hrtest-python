8570dd2ac25667bc06594f30be6d857d258dbdd9
Yoni <yorozen@gmail.com>
2022-07-12 10:03:52 +0300

    updated_with_correct_keys

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from flask import send_from_directory
 
 app = flask.Flask(__name__)
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/favicon.png')
+
 
 @app.route('/')
 @app.route('/home')
@@ -20,13 +22,14 @@ def home():
         host='redis-12000.yonicluster.primary.cs.redislabs.com',
         port=12000,
         ssl=True,
-        ssl_keyfile='./client_key_rs.pem',
-        ssl_certfile='./client_cert_rs.pem',
+        ssl_keyfile='./client.key',
+        ssl_certfile='./client.crt',
         ssl_cert_reqs='required',
         ssl_ca_certs='./proxy_cert.pem',
     )
     info = r.info()
     return info
+
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
