1b35b55d54b4c7192c388ee6178550c686712442
Yoni <yorozen@gmail.com>
2022-07-12 09:42:42 +0300

    updated

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-import redis
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import flask
+import os
+from flask import send_from_directory
 
+app = flask.Flask(__name__)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/favicon.png')
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Hello World"
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
