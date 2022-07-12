b21a37d300f414f7bc782e7b439d94e9b3c8ca34
Yoni <yorozen@gmail.com>
2022-07-12 09:50:03 +0300

    updated_redis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flask
 import os
+
+import redis
 from flask import send_from_directory
 
 app = flask.Flask(__name__)
@@ -12,7 +14,19 @@ def favicon():
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello World"
+    r = redis.Redis(
+        password='redislabs',
+        decode_responses=True,
+        host='redis-12000.yonicluster.primary.cs.redislabs.com',
+        port=12000,
+        ssl=True,
+        ssl_keyfile='./client_key_rs.pem',
+        ssl_certfile='./client_cert_rs.pem',
+        ssl_cert_reqs='required',
+        ssl_ca_certs='./proxy_cert.pem',
+    )
+    info = r.info()
+    return info
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
